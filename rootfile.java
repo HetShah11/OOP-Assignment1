d0ad49aad7c4bed2eb14f901505ea33b562c1867
HetShah11 <68295185+HetShah11@users.noreply.github.com>
2020-10-20 15:57:50 +0530

    this has the main core of program

---
+++ rootfile.java
@@ -0,0 +1,57 @@
+/*
+----Account class----
+data members
+-account number
+-account holder name
+-balance
+
+members Functions
+-constructor - initialize all variables
+-displayInfo - displays current info status of the ACCOUNT
+-deposit - adds money in ACCOUNT
+-withdraw - removes money from ACCOUNT
+*/
+import java.util.Scanner;
+
+ class Account
+ {
+     private long accNum;
+     private String holderName;
+     private double balance;
+
+     Scanner sc = new Scanner(System.in);
+
+     Account()
+     {
+         accNum=0
+         holderName=new String();
+         balance=0.0;
+     }
+
+     void displayInfo()
+     {
+         System.out.println("ACCOUNT NUMBER : " + accNum );
+         System.out.println("ACCOUNT HOLDER : " + holderName );
+         System.out.println("BALANCE : " + balance );
+     }
+
+     void deposit()
+     {
+         double depo;
+         System.out.println("Enter amount you want to deposit - ");
+         depo=sc.nextDouble();
+         //adding assertion and exception handling here
+         this.balance = this.balance+depo;
+         System.out.println("THANK YOU...YOUR AMOUNT HAS BEEN DEPOSITED IN YOUR ACCOUNT\nUPDATED BALANCE : "+this.balance);
+     }
+
+     void withdraw()
+     {
+         double with;
+         System.out.println("Enter amount you want to withdraw - ");
+         depo=sc.nextDouble();
+         //adding assertion and exception handling here
+         this.balance = this.balance-with;
+         System.out.println("THANK YOU FOR YOUR PATIENCE\nUPDATED BALANCE : "+this.balance);
+     }
+ }
